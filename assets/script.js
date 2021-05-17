15b287ef7f5393b4ceb39f63173ab22a21816f00
Isaac Aguirre <isaac.aguirre@du.edu>
2021-05-17 10:23:42 -0600

    update 2.1

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ const $infoBox = document.querySelector(".infoBox");
 const $quitBtn = $infoBox.querySelector(".quit");
 const $continue = $infoBox.querySelector(".continue");
 const $quizBox = document.querySelector(".quizBox");
-
+const $nextBtn = document.querySelector(".nextBtn");
 
 //Start quiz with a click
 $startBtn.onclick = () =>{
@@ -20,10 +20,20 @@ $quitBtn.onclick = () =>{
 $continue.onclick = () =>{
     $infoBox.classList.remove("activeInfo");
     $quizBox.classList.add("activeInfo");
-    showQuestion(0)
+    showQuestion(2);
 }
 
 let queCount = 0;
+
+$nextBtn.onclick =()=>{
+   if(queCount > questions.length - 1) {
+       queCount++;
+       showQuestion(queCount);
+   }else{
+       console.log("question completed")
+   }
+}
+
 
 function showQuestion(index){
     const $queText = document.querySelector(".queText");
@@ -36,4 +46,8 @@ function showQuestion(index){
 
     $queText.innerHTML = queTag;
     $optionList.innerHTML = optionTag;
+}
+
+function optionSelected(answer){
+    let userAnswer = answer.textContent;
 }
