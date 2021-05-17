4e60e4729c7dd648391746afaac31ee8fb1ae2d2
Isaac Aguirre <isaac.aguirre@du.edu>
2021-05-17 02:43:42 -0600

    working unfinished 2.0

---
+++ assets/Options.js
@@ -0,0 +1,47 @@
+//array of questions
+let questions =[
+    {
+        number: 1,
+        question: "What does HTTP stands for?",
+        answer: "Hypertext Preprocessor",
+        options:[
+                 "Hypertext Preprocessor",
+                 "Hippotext Preprocessor",
+                 "Hypertext Proprecessor",
+                 "Hometext Preprocessor",
+                ]
+    },
+    {
+        number: 2,
+        question: "What does CSS stands for?",
+        answer: "Cascading Style Sheets",
+        options:[
+                 "Casper Style Sheets",
+                 "Cascading Stylus Sheets",
+                 "Cascading Sheets Style",
+                 "Cascading Style Sheets",
+                ]
+    },
+    {
+        number: 3,
+        question: "What does .js stands for?",
+        answer: "JavaScript",
+        options:[
+                 "JavaStyle",
+                 "JavaSky",
+                 "JavaScript",
+                 "Javadavadoo",
+                ]
+    },
+    {
+        number: 4,
+        question: "What does HTML stands for?",
+        answer: "Hypertext Markup Language",
+        options:[
+                 "Hypertext Markup Language",
+                 "Hypertext Makeup Language",
+                 "Hoppertext Markup Language",
+                 "Hypertext Markdown Language",
+                ]
+    }
+];
--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ const $startBtn = document.querySelector(".startBtn button");
 const $infoBox = document.querySelector(".infoBox");
 const $quitBtn = $infoBox.querySelector(".quit");
 const $continue = $infoBox.querySelector(".continue");
+const $quizBox = document.querySelector(".quizBox");
 
 
 //Start quiz with a click
@@ -17,5 +18,22 @@ $quitBtn.onclick = () =>{
 
 //continue to questions
 $continue.onclick = () =>{
-    $infoBox.classList.add("activeInfo");
+    $infoBox.classList.remove("activeInfo");
+    $quizBox.classList.add("activeInfo");
+    showQuestion(0)
+}
+
+let queCount = 0;
+
+function showQuestion(index){
+    const $queText = document.querySelector(".queText");
+    const $optionList = document.querySelector(".optionList");
+    let queTag = '<span>'+ questions[index].question +'</span>';
+    let optionTag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[3] +'<span></span></div>'
+
+    $queText.innerHTML = queTag;
+    $optionList.innerHTML = optionTag;
 }
